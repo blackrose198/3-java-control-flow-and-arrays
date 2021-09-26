09531c8b7006e043d3213a221978a6d52b826d43
blackrose198 <grushinskayan@gmail.com>
2021-09-26 21:57:53 +0300

    fix task 6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -4,14 +4,17 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMax(1, 2, 3, 4));
-         */
+
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        //функция для нахождения максимума
+        int max_1 = Math.max(a,b);
+        int max_2 = Math.max(c,d);
+        int max = Math.max(max_1, max_2);
+        return max;
     }
 
 }
