dccaa3b67642c3e4c1a596908e66f440e18e4657
blackrose198 <grushinskayan@gmail.com>
2021-09-26 22:48:19 +0300

    fix task 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -2,17 +2,29 @@ package com.example.task11;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        //функция которая переставляет элементы массива
+        if(arr!=null) {
+            if (arr.length > 0) {
+                int min_index = 0;
+                int min = arr[0];
+                for (int i = 0; i < arr.length; ++i) {
+                    if (arr[i] <= min) {
+                        min = arr[i];
+                        min_index = i;
+                    }
+                }
+                int tmp = arr[0];
+                arr[0] = arr[min_index];
+                arr[min_index] = tmp;
+            }
+        }
     }
 
 }
