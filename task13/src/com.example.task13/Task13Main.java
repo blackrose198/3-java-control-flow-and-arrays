64bf2153effececf9b4da1583f0c2840ba248a57
blackrose198 <grushinskayan@gmail.com>
2021-09-27 00:00:23 +0300

    fix task 13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,35 @@
 package com.example.task13;
 
+import java.util.Arrays;
+
 public class Task13Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
+        int[] arr = new int[]{};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        //функция которая убирает элементы массива, которые больше 1000
+        int[] new_arr = arr;
+        if (arr != null && arr.length > 0) {
+            int size_new_arr = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < 1000) {
+                    size_new_arr++;
+                }
+            }
+            new_arr = new int[size_new_arr];
+            while (size_new_arr != 0) {
+                for (int i = 0; i < arr.length; i++) {
+                    if (arr[i] < 1000) {
+                        new_arr[new_arr.length-size_new_arr] = arr[i];
+                        size_new_arr--;
+                    }
+                }
+            }
+        }
+       return new_arr;
     }
 
 }
